caa32a161fd88de1b90ac611e6f74572a299e1d6
sellamiwael <wael.sellami@gmail.com>
2023-12-13 11:45:02 +0100

    Add files via upload

---
+++ lsarticles.php
@@ -0,0 +1,29 @@
+<html lang="fr">
+<body>
+    <h2>Tableau d'exemple</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Reference</th>
+                <th>Designation</th>
+                <th>Prix</th>
+                <th>Quantite en stock</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>R001</td>
+                <td>Produit A</td>
+                <td>25.99</td>
+                <td>100</td>
+            </tr>
+            <tr>
+                <td>R002</td>
+                <td>Produit B</td>
+                <td>15.49</td>
+                <td>75</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>
